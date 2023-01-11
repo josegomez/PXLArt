9f4f168759e4709752ac94f160b606d3a7f5f465
Jose Gomez <jose@josecgomez.com>
2023-01-11 11:55:19 -0500

    Add project files.

---
+++ PXLArt/frmPLArt.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace PXLArt
+{
+    partial class XLPixelArtGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreatePXLArt = new System.Windows.Forms.Button();
+            this.picPic = new System.Windows.Forms.PictureBox();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreatePXLArt
+            // 
+            this.btnCreatePXLArt.Location = new System.Drawing.Point(12, 304);
+            this.btnCreatePXLArt.Name = "btnCreatePXLArt";
+            this.btnCreatePXLArt.Size = new System.Drawing.Size(114, 23);
+            this.btnCreatePXLArt.TabIndex = 0;
+            this.btnCreatePXLArt.Text = "Convert Image";
+            this.btnCreatePXLArt.UseVisualStyleBackColor = true;
+            this.btnCreatePXLArt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picPic
+            // 
+            this.picPic.Location = new System.Drawing.Point(12, 12);
+            this.picPic.Name = "picPic";
+            this.picPic.Size = new System.Drawing.Size(284, 284);
+            this.picPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPic.TabIndex = 1;
+            this.picPic.TabStop = false;
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Location = new System.Drawing.Point(12, 333);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(283, 23);
+            this.pbProgress.TabIndex = 2;
+            // 
+            // XLPixelArtGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 368);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.picPic);
+            this.Controls.Add(this.btnCreatePXLArt);
+            this.MinimizeBox = false;
+            this.Name = "XLPixelArtGenerator";
+            this.Text = "XL Pixel Art Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.picPic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreatePXLArt;
+        private System.Windows.Forms.PictureBox picPic;
+        private System.Windows.Forms.ProgressBar pbProgress;
+    }
+}
+
---
+++ PXLArt/frmPLArt.cs
@@ -0,0 +1,157 @@
+﻿
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PXLArt
+{
+    public partial class XLPixelArtGenerator : Form
+    {
+        public XLPixelArtGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter= "Bitmap Images|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.ico;*.tif;*.tiff|All Files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                Image image = Image.FromFile(ofd.FileName);
+                int xX = picPic.Width, yY = picPic.Height;
+                int newWidth, newHeight;
+                if (image.Width > xX || image.Height > yY)
+                {
+                    if (image.Width > image.Height)
+                    {
+                        newWidth = xX;
+                        newHeight = (int)(image.Height * ((float)xX / image.Width));
+                    }
+                    else
+                    {
+                        newWidth = (int)(image.Width * ((float)yY / image.Height));
+                        newHeight = yY;
+                    }
+                    Image newImage = new Bitmap(newWidth, newHeight);
+                    using (var graphics = Graphics.FromImage(newImage))
+                    {
+                        graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+                    }
+                    image.Dispose();
+                    image = newImage;
+                }
+                picPic.Image = image;
+                
+                // Get the width and height of the image
+                int width = image.Width;
+                int height = image.Height;
+                pbProgress.Maximum = (width * height)+width+height;
+                pbProgress.Step = 1;
+                Task.Run(() =>
+                {
+                // create new Excel package
+                using (ExcelPackage excelPackage = new ExcelPackage())
+                {
+                    // add a new worksheet to the empty workbook
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("PixelArt");
+                    Bitmap imageB = new Bitmap(image);
+                    BitmapData imageData = imageB.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                    // Get the address of the first pixel
+                    IntPtr pointer = imageData.Scan0;
+                    // Declare an array to hold the bytes of the image
+                    int bytes = Math.Abs(imageData.Stride) * height;
+                    byte[] data = new byte[bytes];
+                    // Copy the pixels to the array
+                    System.Runtime.InteropServices.Marshal.Copy(pointer, data, 0, bytes);
+                    // Unlock the bits of the image
+                    imageB.UnlockBits(imageData);
+
+                        List<Tuple<int, int>> listOfCordinates = new List<Tuple<int, int>>();
+                    // Loop through the rows and columns of the image
+                    for (int row = 1; row <= height; row++)
+                    {
+                        for (int col = 1; col <= width; col++)
+                        {
+                            
+                            int pixelIndex = ((row - 1) * width + (col - 1)) * 4;
+                            Color pixelColor = Color.FromArgb(data[pixelIndex + 3], data[pixelIndex + 2], data[pixelIndex + 1], data[pixelIndex]);
+                            // Set the cell color to the pixel color
+                            worksheet.Cells[row, col].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                            worksheet.Cells[row, col].Style.Fill.BackgroundColor.SetColor(pixelColor);
+                            try
+                            {
+                                this.Invoke((ThreadStart)delegate ()
+                                {
+                                    pbProgress.PerformStep();
+                                });
+                            }
+                            catch { }
+                        }
+                    }
+                        
+
+
+                        
+                        // set the column width for all columns to be square
+                        for (int i = 1; i <= width; i++)
+                        {
+                            worksheet.Column(i).Width = 2.14;
+                            try
+                            {
+                                this.Invoke((ThreadStart)delegate ()
+                                {
+                                    pbProgress.PerformStep();
+                                });
+                            }
+                            catch { }
+                        }
+                        for (int i = 1; i <= height; i++)
+                        {
+                            worksheet.Row(i).Height = 15;
+                            try
+                            {
+                                this.Invoke((ThreadStart)delegate ()
+                                {
+                                    pbProgress.PerformStep();
+                                });
+                            }
+                            catch { }
+                        }
+                        worksheet.View.ZoomScale = 25;
+                        // Save the workbook to a memory stream
+                        MemoryStream stream = new MemoryStream();
+                        excelPackage.SaveAs(stream);
+                        // write memory stream to a file
+                        //
+                        this.Invoke((ThreadStart)delegate ()
+                        {
+                            SaveFileDialog sfd = new SaveFileDialog();
+                            sfd.Filter = "Excel Files|*.xls;*.xlsx|All Files (*.*)|*.*";
+                            sfd.DefaultExt = ".xlsx";
+                            sfd.FileName = Path.GetFileNameWithoutExtension(ofd.FileName);
+                            if (sfd.ShowDialog() == DialogResult.OK)
+                            {
+                                File.WriteAllBytes(sfd.FileName, stream.ToArray());
+                                Process.Start(sfd.FileName);
+                            }
+                        });
+                    }
+                });
+            }
+
+        }
+
+    }
+}
